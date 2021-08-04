f75127beca2ec2721f5ab7c55833a1688de8be85
israelhrm1995 <isrealhrm1995@gmail.com>
2021-08-03 22:49:31 -0300

    MongoDB com repository e service

---
+++ src/main/java/com/israelspring/mongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.israelspring.mongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.israelspring.mongo.domain.User;
+
+@Repository
+public interface UserRepository  extends MongoRepository<User, String>{
+}
