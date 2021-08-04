27e7b6847291df649d3428df37c0fde8d3683ad3
israelhrm1995 <isrealhrm1995@gmail.com>
2021-08-04 00:48:50 -0300

    Atualização de usuário com PUT

--- src/main/java/com/israelspring/mongo/services/UserService.java
+++ src/main/java/com/israelspring/mongo/services/UserService.java
@@ -13,34 +13,44 @@ import com.israelspring.mongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
-	
+
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
-		if(user.isPresent()) {
+		if (user.isPresent()) {
 			User newUser = user.get();
 			return newUser;
-		}else {
+		} else {
 			throw new ObjectNotFoundException("Objeto não encontrado");
 		}
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
 	}
-	
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setEmail(obj.getEmail());
+		newObj.setName(obj.getName());
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getName());
 	}
